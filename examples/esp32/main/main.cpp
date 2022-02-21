9ae0034e88525b24d1cb3253dc3258c681bf81d1
Robert Manzke <robert.manzke@fh-kiel.de>
2022-02-21 19:49:50 +0100

    motor control

--- examples/esp32/main/main.cpp
+++ examples/esp32/main/main.cpp
@@ -7,9 +7,36 @@
 #include <freertos/task.h>
 #include <nvs_flash.h>
 #include <protocol_examples_common.h>
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
 
 #define LED GPIO_NUM_5
 #define PRINT_LINK_STATE false
+
+#define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
+
+static void mcpwm_example_gpio_initialize(void)
+{
+    printf("initializing mcpwm servo control gpio......\n");
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 18);    //Set GPIO 18 as PWM0A, to which servo is connected
+}
+
+/**
+ * @brief Use this function to calcute pulse width for per degree rotation
+ *
+ * @param  degree_of_rotation the angle in degree to which servo has to rotate
+ *
+ * @return
+ *     - calculated pulse width
+ */
+static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
+{
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + (((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
 
 unsigned int if_nametoindex(const char* ifName)
 {
@@ -83,14 +110,25 @@ void tickTask(void* userParam)
   }
 
   gpio_set_direction(LED, GPIO_MODE_OUTPUT);
-
+  uint32_t angle = 0;
+  bool updated = false;
   while (true)
   {
     xSemaphoreTake(handle, portMAX_DELAY);
 
     const auto state = link.captureAudioSessionState();
-    const auto phase = state.phaseAtTime(link.clock().micros(), 1.);
+    const auto phase = state.phaseAtTime(link.clock().micros(), 1.f);
     gpio_set_level(LED, !(fmodf(phase, 1.f) < 0.1f));
+    if((fmodf(phase, 1.f) < 0.1f)){
+        if(!updated){
+            angle += 20;
+            angle %= 80;
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, servo_per_degree_init(angle));
+            updated = true;
+        }
+    }else{
+        updated = false;
+    }
     portYIELD();
   }
 }
@@ -105,5 +143,20 @@ extern "C" void app_main()
   SemaphoreHandle_t tickSemphr = xSemaphoreCreateBinary();
   timerGroup0Init(500, tickSemphr);
 
-  xTaskCreate(tickTask, "tick", 8192, tickSemphr, configMAX_PRIORITIES - 1, nullptr);
+
+    //1. mcpwm gpio initialization
+    mcpwm_example_gpio_initialize();
+
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm......\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+
+
+    xTaskCreate(tickTask, "tick", 8192, tickSemphr, configMAX_PRIORITIES - 1, nullptr);
 }
